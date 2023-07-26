d5965d4d139c8e05f149fbb40c413eef30a407d4
Jose <jvaldebenitog@me.com>
2023-07-25 22:20:48 -0400

    finish hero

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import "../styles/home.css";
 
 function Home() {
   return (
-    <section className="Hero">
-      <h1>José Valdebenito</h1>
-      <h3>Soy desarrollador</h3>
+    <section className="hero d-flex">
+      <div className="hero-wrap text-center flex-column">
+        <div className="hero-info-wrap p-3">
+          <h1 className="hero-title">José Valdebenito</h1>
+          <h3 className="hero-subtitle">Soy desarrollador</h3>
+          <a className="hero-btn" href="contacto">
+            Contactame!
+          </a>
+        </div>
+        <div>
+          <section className="social-icons d-flex flex-wrap align-items-center justify-content-center m-4">
+            <a
+              href="https://twitter.com/JValdebenitoG"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-twitter"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/blackshirtdev/"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a
+              href="https://github.com/JoseValdebenitoG"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jvaldebenitog/"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </section>
+        </div>
+      </div>
     </section>
   );
 }
