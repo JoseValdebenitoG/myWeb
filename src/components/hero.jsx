fca34514857db6199836e1f81094fe2f76c34158
Jose <jvaldebenitog@me.com>
2023-07-26 21:34:22 -0400

    add carousel

---
+++ src/components/hero.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero d-flex">
+      <div className="hero-wrap text-center flex-column">
+        <div className="hero-info-wrap p-3">
+          <h1 className="hero-title">José Valdebenito</h1>
+          <h3 className="hero-subtitle">Soy desarrollador</h3>
+          <a className="hero-btn" href="contacto">
+            Contactame!
+          </a>
+        </div>
+        <div>
+          <section className="social-icons d-flex flex-wrap align-items-center justify-content-center m-4">
+            <a
+              href="https://twitter.com/JValdebenitoG"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-twitter"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/blackshirtdev/"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a
+              href="https://github.com/JoseValdebenitoG"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jvaldebenitog/"
+              className="social-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,54 +1,13 @@
 import React from "react";
 import "../styles/home.css";
+import Hero from "../components/hero";
+import Carousel from "../components/carousel";
 
 function Home() {
   return (
-    <section className="hero d-flex">
-      <div className="hero-wrap text-center flex-column">
-        <div className="hero-info-wrap p-3">
-          <h1 className="hero-title">José Valdebenito</h1>
-          <h3 className="hero-subtitle">Soy desarrollador</h3>
-          <a className="hero-btn" href="contacto">
-            Contactame!
-          </a>
-        </div>
-        <div>
-          <section className="social-icons d-flex flex-wrap align-items-center justify-content-center m-4">
-            <a
-              href="https://twitter.com/JValdebenitoG"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-twitter"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/blackshirtdev/"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a
-              href="https://github.com/JoseValdebenitoG"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/jvaldebenitog/"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-linkedin"></i>
-            </a>
-          </section>
-        </div>
-      </div>
+    <section>
+      <Hero />
+      <Carousel />
     </section>
   );
 }
